597029caadc73537ee48f617651b82a054387764
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2025-03-09 12:17:25 +1100

    feat: 11

---
+++ two-pointers/11-container-with-most-water.cpp
@@ -0,0 +1,27 @@
+// Solution: to make sure that all relevant options are explored, we use two pointers to
+// keep track of the current area. Whichever pointer has a smaller value is incremented
+// towards the middle, to make sure that all areas of increasing size are explored.
+// Requires O(n) time (all heights iterated) and O(1) space (no n dependent alloations made).
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxArea(const std::vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+
+        int result = 0;
+        while (l < r) {
+            std::cout << "left: " << l << ", right: " << r << "\n";
+            result = std::max(result, (r - l) * std::min(height[l], height[r]));
+            if (height[l] < height[r]) {
+                ++l;
+            } else {
+                --r;
+            }
+        }
+        return result;
+    }
+};
